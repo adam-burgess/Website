507e0971d2ce0acf2b57f4e656856312ef5846ad
Adam <adamburgess40@gmail.com>
2026-09-24 19:24:32 +0800

    Initial Website

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+// Astro's main config file. Read once when you run dev or build.
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // The live URL. Used to build absolute links (RSS, canonical tags, sitemap).
+  site: 'https://jumari.com.au',
+});
